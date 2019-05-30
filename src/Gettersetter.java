0662061fa7deb505562f64593dba98eb0443a3d0
sindhuvinoth <sindhu.sparkling@gmail.co>
2019-05-30 18:09:08 -0500

    Modified Msg

--- src/Gettersetter.java
+++ src/Gettersetter.java
@@ -3,7 +3,7 @@ public class Gettersetter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Thank you");
 		Encapsulation getset = new Encapsulation();
 		getset.setName("Sindhu");
 		System.out.println(getset.getName());
