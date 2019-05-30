5964cc6d88b8b1a863a3d13348514631e23fcccf
Sindhuvinoth <51073453+Sindhuvinoth@users.noreply.github.com>
2019-05-30 18:44:46 -0500

    Update Gettersetter.java

--- src/Gettersetter.java
+++ src/Gettersetter.java
@@ -2,8 +2,8 @@
 public class Gettersetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Thank you");
+		System.out.println("For using GitHub");
 		Encapsulation getset = new Encapsulation();
 		getset.setName("Sindhu");
 		System.out.println(getset.getName());
